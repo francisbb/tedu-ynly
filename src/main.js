e9b57ddbdca5892ad6ad8d40e4804426a96484a7
AoYangFei <freeit.victor@gmail.com>
2021-02-09 02:52:50 +0800

    AoYangFei

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-
+import axios from 'axios'
 Vue.config.productionTip = false
 
 new Vue({
